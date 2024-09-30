13ce12400caf08d5d26d53d5144f4ff053c580c7
Mpetof <124488987+Mpetof@users.noreply.github.com>
2024-09-30 09:53:35 +0800

    Add files via upload

---
+++ demo_web_page/tree.js
@@ -0,0 +1,58 @@
+var svg = d3.select("#my_dataviz")
+.append("svg")
+.attr("width", document.querySelector("#my_dataviz").offsetWidth) // 使用容器宽度
+.attr("height", 400) // 固定高度
+
+var g = svg.append("g")
+    .attr("transform", "translate(-60, -10)");
+
+var nodes = [
+    { id: 1, name: "Premise 1", x: 100, y: 100 },
+    { id: 2, name: "Premise 2", x: 300, y: 100 },
+    { id: 3, name: "Conclusion", x: 200, y: 300 }
+];
+
+var links = [
+    { source: nodes[0], target: nodes[2] },
+    { source: nodes[1], target: nodes[2] }
+];
+
+links.forEach(function(linkData) {
+    g.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "#aaa")
+        .attr("stroke-width", 2)
+        .attr("d", d3.linkVertical()
+            .x(function(d) { return d.x; })
+            .y(function(d) { return d.y; })(linkData));
+});
+
+nodes.forEach(function(nodeData) {
+    g.append("circle")
+        .attr("r", 10)
+        .attr("fill", "#69b3a2")
+        .attr("cx", nodeData.x)
+        .attr("cy", nodeData.y)
+        .on("mouseover", function() {
+            if (nodeData.id === 1) { 
+                document.querySelector(".premise1").style.color = "red";
+            }
+            if (nodeData.id === 2) { 
+                document.querySelector(".premise2").style.color = "red";
+            }
+            if (nodeData.id === 3) { 
+                document.querySelector(".conclusion").style.color = "red";
+            }
+        })
+        .on("mouseout", function() {
+            document.querySelector(".premise1").style.color = "black";
+            document.querySelector(".premise2").style.color = "black";
+            document.querySelector(".conclusion").style.color = "black";
+        });
+
+    g.append("text")
+        .attr("class", "node-label")
+        .attr("x", nodeData.x + 15)
+        .attr("y", nodeData.y + 5)
+        .text(nodeData.name);
+});
